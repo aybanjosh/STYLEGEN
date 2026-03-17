9f90f3a136eed3c84f5e82d3b833c9c498cde420
aybanjosh <135392076+aybanjosh@users.noreply.github.com>
2026-03-17 18:47:37 +0800

    Add files via upload

---
+++ stylegen_db.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2026 at 11:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stylegen_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table stylegen_db.cart: #1932 - Table &#039;stylegen_db.cart&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`cart`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gcash_payment`
+--
+-- Error reading structure for table stylegen_db.gcash_payment: #1932 - Table &#039;stylegen_db.gcash_payment&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.gcash_payment: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`gcash_payment`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table stylegen_db.orders: #1932 - Table &#039;stylegen_db.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+-- Error reading structure for table stylegen_db.order_items: #1932 - Table &#039;stylegen_db.order_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`order_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table stylegen_db.products: #1932 - Table &#039;stylegen_db.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+-- Error reading structure for table stylegen_db.reviews: #1932 - Table &#039;stylegen_db.reviews&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.reviews: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`reviews`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_contact`
+--
+-- Error reading structure for table stylegen_db.tbl_contact: #1932 - Table &#039;stylegen_db.tbl_contact&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.tbl_contact: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`tbl_contact`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_products`
+--
+-- Error reading structure for table stylegen_db.tbl_products: #1932 - Table &#039;stylegen_db.tbl_products&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.tbl_products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`tbl_products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sg`
+--
+-- Error reading structure for table stylegen_db.tbl_sg: #1932 - Table &#039;stylegen_db.tbl_sg&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.tbl_sg: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`tbl_sg`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table stylegen_db.users: #1932 - Table &#039;stylegen_db.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table stylegen_db.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `stylegen_db`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
